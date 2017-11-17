cf0e5109721bb41ee241c8c6ebb6cbc46929d241
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 20:12:23 -0600

    Se ingresan los valores de la ecuacion y se saca su formula general

---
+++ Ecuacion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecuacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La ecuacion debe tener tres elementos: ");
+            Console.WriteLine("a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("c: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("x°1: " + (Positivo(a, b, c) / (2 * a)));
+            Console.WriteLine("x°2: " + (Negativo(a, b, c) / (2 * a)));
+            Console.ReadKey();
+        }
+
+        static double Positivo(double a, double b, double c)
+        {
+            return ((0 - 1) * b) + (Math.Sqrt((Math.Pow(b, 2) - (4 * a * c))));
+
+        }
+
+        static double Negativo(double a, double b, double c)
+        {
+            return ((0 - 1) * b) - (Math.Sqrt((Math.Pow(b, 2) - (4 * a * c))));
+        }
+    }
+}
